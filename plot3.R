0b258c1d2335474aec33c7ab51be4a199921f211
Arthur McDonald <arthur@Arthurs-MacBook-Air.local>
2022-02-22 19:30:29 -0700

    fix: some issues with plots 2 and 3

--- plot3.R
+++ plot3.R
@@ -14,11 +14,12 @@ subset_df <- df[df$Date > as.Date("2007-01-31") &
 png(file="plot3.png", width=480, height=480)
 plot(subset_df$Time, subset_df$Sub_metering_1, type="l", col="black", 
      ylab="Energy sub metering", xlab="", xaxt='n')
-lines(subset_df$Time, subset_df$Sub_metering_2, col="red", type="l")
-lines(subset_df$Time, subset_df$Sub_metering_3, col="blue", type="l")
 axis(1, at=c(0,0.5,1), labels=c("Thu", "Fri", "Sat"))
 legend(x = "topright", legend=c("Sub_metering_1", "Sub_metering_2", 
                                 "Sub_metering_3"),
        bty = "n", lty=c(1,1,1),
        col=c("black", "red", "blue"))
+lines(subset_df$Time, subset_df$Sub_metering_2, col="red", type="l")
+lines(subset_df$Time, subset_df$Sub_metering_3, col="blue", type="l")
+
 dev.off()
